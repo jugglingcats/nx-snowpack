e2481daf595d9db79040688da211cd5762f6129f
jugglingcats <alfie@glowbuzzer.com>
2020-10-30 14:20:39 +0000

    Initial commit

---
+++ libs/mylib/src/lib/mylib.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Mylib from './mylib';
+
+describe('Mylib', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Mylib />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/mylib/src/lib/mylib.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './mylib.css';
+
+/* eslint-disable-next-line */
+export interface MylibProps {}
+
+export const Mylib = (props: MylibProps) => {
+  return (
+    <div>
+      <h1>Welcome to mylib!</h1>
+    </div>
+  );
+};
+
+export default Mylib;
